e9fe2157f9c9f96d26dd2259b4b73f364072a165
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-11 17:11:33 +0100

    feat: structure done

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,49 @@ import tasks from "./data.js";
 
 function App() {
   
-  const completedTasks = tasks.filter((task) => task.state === `completed`);
-  const openTasks = tasks.filter((task) => task.state !== `completed`);
-  console.log(openTasks);
+  const completedTasks = tasks.filter((task) => task.state === "completed");
+  const openTasks = tasks.filter((task) => task.state !== "completed");
   
 
   return (
     <>
       <h1>Task Manager</h1>
-      <h3>Current Tasks ()</h3>
+      <h3>Current Tasks ({openTasks.length})</h3>
       <ul>
-        {openTasks.map((curTask, index) => (<li key={index}>{curTask}</li>))}
+        {
+          openTasks.map((curTask, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <p><strong>{curTask.title}</strong></p>
+                  <span>{curTask.state}</span>
+                </div>
+                <p>Priority: {curTask.priority}</p>
+                <p>Est. time {curTask.estimatedTime}</p>
+                </li>
+            )
+          })
+        }
       </ul>
 
-      <h3>Completed Tasks ()</h3>
+<hr />
+
+      <h3>Completed Tasks ({completedTasks.length})</h3>
       <ul>
-        {completedTasks.map((curTask, index) => (<li key={index}>{curTask}</li>))}
+        {
+          completedTasks.map((curTask, index) => {
+            return (
+            <li key={index}>
+              <div>
+                  <p><strong>{curTask.title}</strong></p>
+                  <span>{curTask.state}</span>
+              </div>
+              <p>Priority: {curTask.priority}</p>
+              <p>Est. time {curTask.estimatedTime}</p>
+              </li>
+            )
+          })
+        }
       </ul>
   
       
