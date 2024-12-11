e926c3938b27eb9673ce93e84bb9324b5b8e2647
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-11 17:46:00 +0100

    feat: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,44 +8,49 @@ function App() {
 
   return (
     <>
-      <h1>Task Manager</h1>
-      <h3>Current Tasks ({openTasks.length})</h3>
-      <ul>
-        {
-          openTasks.map((curTask, index) => {
-            return (
+      <header>
+        <h1>Task Manager</h1>
+      </header>
+      <main>
+        <h3>Current Tasks ({openTasks.length})</h3>
+        <ul>
+          {
+            openTasks.map((curTask, index) => {
+              return (
+                <li key={index}>
+                  <div className="task-title">
+                    <p><strong>{curTask.title}</strong></p>
+                    <span className="status">{curTask.state}</span>
+                  </div>
+                  <p>Priority: {curTask.priority}</p>
+                  <p>Est. time {curTask.estimatedTime}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+
+        <hr />
+
+        <h3>Completed Tasks ({completedTasks.length})</h3>
+        <ul>
+          {
+            completedTasks.map((curTask, index) => {
+              return (
               <li key={index}>
-                <div>
-                  <p><strong>{curTask.title}</strong></p>
-                  <span>{curTask.state}</span>
+                <div className="task-title">
+                    <p><strong>{curTask.title}</strong></p>
+                    <span className="status completed">{curTask.state}</span>
                 </div>
                 <p>Priority: {curTask.priority}</p>
                 <p>Est. time {curTask.estimatedTime}</p>
-                </li>
-            )
-          })
-        }
-      </ul>
-
-<hr />
-
-      <h3>Completed Tasks ({completedTasks.length})</h3>
-      <ul>
-        {
-          completedTasks.map((curTask, index) => {
-            return (
-            <li key={index}>
-              <div>
-                  <p><strong>{curTask.title}</strong></p>
-                  <span>{curTask.state}</span>
-              </div>
-              <p>Priority: {curTask.priority}</p>
-              <p>Est. time {curTask.estimatedTime}</p>
               </li>
-            )
-          })
-        }
-      </ul>
+              )
+            })
+          }
+        </ul>
+      </main>
+      
   
       
 
